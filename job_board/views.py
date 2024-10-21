e284bffd53fd9c850c2b8a1cd2b0cf7674346268
Mateusz Brychczynski <mat.brych@gmail.com>
2024-10-21 10:46:56 +0100

    Handling "does not exist" error for Job Details

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import JobPosting
 
 # Create your views here.
@@ -8,6 +8,6 @@ def index(request):
     return render(request, 'job_board/index.html', context)
 
 def job_detail(request, pk):
-    job_posting = JobPosting.objects.get(pk=pk)
+    job_posting = get_object_or_404(JobPosting, pk=pk, is_active=True)
     context = {'posting': job_posting}
     return render(request, 'job_board/detail.html', context)
