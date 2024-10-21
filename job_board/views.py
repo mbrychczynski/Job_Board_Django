4e870844ad9db5c73b9db33a53b67b4f9ea09b32
Mateusz Brychczynski <mat.brych@gmail.com>
2024-10-21 10:43:52 +0100

    job detail pages

--- job_board/views.py
+++ job_board/views.py
@@ -6,3 +6,8 @@ def index(request):
     active_postings = JobPosting.objects.filter(is_active=True)
     context = {'job_postings': active_postings}
     return render(request, 'job_board/index.html', context)
+
+def job_detail(request, pk):
+    job_posting = JobPosting.objects.get(pk=pk)
+    context = {'posting': job_posting}
+    return render(request, 'job_board/detail.html', context)
